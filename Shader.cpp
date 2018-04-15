3d0ee9b733fa7b92a8820dcc3c20209b46cb5082
erodriguezg <erodriguez.cl@gmail.com>
2018-04-15 16:20:51 -0300

    terminada introduccion de shaders con utilitarios shaders

--- Shader.cpp
+++ Shader.cpp
@@ -8,16 +8,54 @@ static void checkShaderError(GLuint shader, GLuint flag, bool isProgram, const s
 
 static std::string loadShader(const std::string &fileName);
 
-Shader::Shader(const std::string &fileName) {
+static GLuint createShader(const std::string &text, GLenum shaderType);
 
+Shader::Shader(const std::string &fileName) {
+    this->program = glCreateProgram();
+    this->shaders[0] = createShader(loadShader(fileName + ".vs.glsl"), GL_VERTEX_SHADER);
+    this->shaders[1] = createShader(loadShader(fileName + ".fs.glsl"), GL_FRAGMENT_SHADER);
+
+    for (unsigned int i = 0; i < NUM_SHADERS; i++) {
+        glAttachShader(this->program, this->shaders[i]);
+    }
+
+    glLinkProgram(this->program);
+    checkShaderError(this->program, GL_LINK_STATUS, true, "Error: Program linking failed: ");
+
+    glValidateProgram(this->program);
+    checkShaderError(this->program, GL_VALIDATE_STATUS, true, "Error: Program is invalid: ");
 }
 
 Shader::~Shader() {
-
+    for (unsigned int i = 0; i < NUM_SHADERS; i++) {
+        glDetachShader(this->program, this->shaders[i]);
+        glDeleteShader(this->shaders[i]);
+    }
+    glDeleteProgram(this->program);
 }
 
 void Shader::bind() {
+    glUseProgram(this->program);
+}
 
+static GLuint createShader(const std::string &text, GLenum shaderType) {
+    GLuint shader = glCreateShader(shaderType);
+    if(shader == 0) {
+        std::cerr << "Error: Shader creation failed!" << std::endl;
+    }
+
+    const GLchar *shaderSourceStrings[1];
+    GLint shaderSourceStringLength[1];
+
+    shaderSourceStrings[0] = text.c_str();
+    shaderSourceStringLength[0] = text.length();
+
+    glShaderSource(shader, 1, shaderSourceStrings, shaderSourceStringLength);
+    glCompileShader(shader);
+
+    checkShaderError(shader, GL_COMPILE_STATUS, false, "Error: Shader compilation failed: ");
+
+    return shader;
 }
 
 static std::string loadShader(const std::string &fileName) {
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "Display.h"
+#include "Shader.h"
 
 int main() {
     Display display(800, 600, "Hello world");
+    Shader shader("./res/basicShader");
     while (!display.isClosed()) {
         display.clear(0.0f, 0.15f, 0.3f, 1.0f);
+        shader.bind();
         display.update();
     }
     return 0;
